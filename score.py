5cdaaca604664d1cb2ab43d6e4e052af3d831dc3
hueiwunhe <ellen30301@gmail.com>
2017-12-10 20:27:04 +0000

    first push

---
+++ score.py
@@ -0,0 +1,21 @@
+record = {}
+
+
+record['Kari'] = 100
+record['Kari'] = 440
+record['Kari'] = 0
+record['Kari'] = 0
+record['Kari'] = 0
+record['Kari'] = 150
+record['Kari'] = 100
+record['Awu'] = 100
+record['Awu'] = 100
+record['Ellen'] = 100
+record['Ellen'] = 20
+record['Ellen'] = 80
+
+record['Ellen'] = 0
+record['Kari'] = 200
+record['Kari'] = 100
+record['Angela'] = 100
+record['Angela'] = 0
